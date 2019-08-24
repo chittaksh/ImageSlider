8eb239694d14184b3f28ba77ba0488582b132843
Chittaksh <khadse.chittaksh@gmail.com>
2019-08-24 15:07:50 -0700

    need to find a way to remote intialise the code on primary display.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from Screens.start_screen import ImageSlider
-import os
-os.environ["KIVY_BCM_DISPMANX_ID"] = "2"
+from Screens.start_screen import 
+
+# Find a way to get access to HDMI on raspberry pi and start slide show remotely. 
+#import os
+#os.environ["KIVY_BCM_DISPMANX_ID"] = "2"
 
 if __name__ == '__main__':
     print('This is the init for the main.')
